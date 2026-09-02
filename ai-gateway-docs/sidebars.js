0d6d19e89a695a6593fb721fbb0a7b6277e4b04f
gitplayer4 <147137879+gitplayer4@users.noreply.github.com>
2026-09-02 14:03:37 +0800

    Add files via upload

---
+++ ai-gateway-docs/sidebars.js
@@ -0,0 +1,9 @@
+// @ts-check
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  aiGateway: [
+    { type: "doc", id: "index", label: "AI 网关" },
+    { type: "doc", id: "quick_start", label: "快速入门" },
+  ],
+};
+export default sidebars;
